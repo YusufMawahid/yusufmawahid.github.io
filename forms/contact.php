cf1e9ba22c661cbef821650c9b234295ae09316a
YusufMawahid <wahidajjah52@gmail.com>
2020-09-08 06:29:56 +0700

    Initial commit

--- forms/contact.php
+++ forms/contact.php
@@ -1,9 +1,9 @@
 <?php 
 $mail = new Nette\Mail\Message;
-$mail->setFrom($_POST['email'])
+$mail->setFrom($_GET['email'])
     ->addTo('yusufmawahid100@gmail.com')
-    ->setSubject($_POST['subject'])
-    ->setBody($_POST['message']);
+    ->setSubject($_GET['subject'])
+    ->setBody($_GET['message']);
 
 $mailer = new Nette\Mail\SendmailMailer;
 $mailer->send($mail);
